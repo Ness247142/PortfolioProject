6b79565518f79b8f372bb1bcbce461a5e52a61fc
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-04-07 18:13:16 +0200

    Delete portfolio_app directory

--- portfolio_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfolioAppConfig(AppConfig):
-    name = 'portfolio_app'
--- portfolio_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-
-class AddProjectForm(forms.ModelForm):
-    class Meta:
-        model = Project
-        fields = '__all__'
